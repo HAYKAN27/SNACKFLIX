ead7e264f73917bf2d4cf89d2d4262095a4b82f7
iRandH <haykansyahputra27@gmail.com>
2025-10-23 19:22:01 +0700

    Aku Menambahka Notif Keranjang dan sosial Media

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
  /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./index.html"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+module.exports = {
+  content: [
+    "./main/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
